731a215e754e913b0fba92007579ee7e45aaad05
Spencer Caldwell <spencer.caldwell@gmail.com>
2017-04-08 13:03:29 -0700

    finished platform specific styling for ios

--- task_row/render.ios.js
+++ task_row/render.ios.js
@@ -2,12 +2,23 @@ import React, {Component} from 'react';
 import {
   View,
   Text,
-  TouchableHighlight,
+  StyleSheet,
 } from 'react-native';
 
-var Swipeout = require('react-native-swipeout')
+import Swipeout from 'react-native-swipeout';
 
-export default function render(styles) {
+const localStyle = StyleSheet.create({
+  row: {
+    marginBottom: 0,
+    marginLeft: 0,
+    marginRight: 0,
+  },
+  container: {
+    marginBottom: 20,
+  },
+});
+
+export default function render(baseStyles) {
 
   const buttons = [
     {
@@ -18,24 +29,18 @@ export default function render(styles) {
     },
   ];
 
-  // <Swipeout
-  //   backgroundColor='#fff'
-  //   right={buttons}
-  //   >
-
   return (
-
-      <View style={styles.container}>
-        <Text style={styles.label}>ios: {this.props.todo.task}</Text>
-
-        <TouchableHighlight
-          onPress={this.onDonePressed.bind(this)}
-          style={styles.doneButton}
-          >
-          <Text>
-            Done
-          </Text>
-        </TouchableHighlight>
-      </View>
+    <View
+      style={localStyle.container}
+      >
+      <Swipeout
+        backgroundColor='#fff'
+        right={buttons}
+        >
+        <View style={[baseStyles.container, localStyle.row]}>
+          <Text style={baseStyles.label}> {this.props.todo.task}</Text>
+        </View>
+      </Swipeout>
+    </View>
   );
 }
